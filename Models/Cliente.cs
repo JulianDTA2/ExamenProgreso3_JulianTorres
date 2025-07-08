27eaff5c53810a43aef5aa5a41258331a6913d89
JulianTorresDev <julian.torres.dev@gmail.com>
2025-07-08 07:28:00 -0500

    Commit 1: creacion de la carpeta models y las clases cliente y logEntry, Creacion de la carpeta viewmodels y la clase cliente view model

---
+++ Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgreso3_JulianTorres.Models
+{
+    public class Cliente
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Empresa { get; set; }
+        public int AntiguedadMeses { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ Models/LogEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgreso3_JulianTorres.Models
+{
+    public class LogEntry
+    {
+        public string Mensaje { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public string FechaFormateada => Fecha.ToString("dd/MM/yyyy HH:mm");
+    }
+}
