779fd79e80f43cedc45d7661f813617ef1260426
JulianTorresDev <julian.torres.dev@gmail.com>
2025-07-08 07:12:07 -0500

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenProgreso3_JulianTorres
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
